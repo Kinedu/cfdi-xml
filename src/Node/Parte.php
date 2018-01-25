1ee7f3beecd115538e13cc18f7daab76bc883456
Orlando Charles <orlando@kinedu.com>
2018-01-25 09:13:26 -0600

    Add parte node

---
+++ src/Node/Parte.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the cfdi-xml project.
+ *
+ * (c) Kinedu
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kinedu\CfdiXML\Node;
+
+use Kinedu\CfdiXML\Common\Node;
+
+class Parte extends Node
+{
+    /**
+     * Node name.
+     *
+     * @var string
+     */
+    protected $nodeName = 'cfdi:Parte';
+}
