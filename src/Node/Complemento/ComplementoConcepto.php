7f1ea6b3531391079e981fe4c5462d191406159c
Orlando Charles <orlando@kinedu.com>
2018-08-28 10:31:02 -0500

    Add IEDU node

---
+++ src/Node/Complemento/ComplementoConcepto.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the cfdi-xml project.
+ *
+ * (c) Kinedu
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kinedu\CfdiXML\Node\Complemento;
+
+use Kinedu\CfdiXML\Common\Node;
+
+class ComplementoConcepto extends Node
+{
+    /**
+     * Define the parent node name.
+     *
+     * @var string
+     */
+    protected $parentNodeName = 'cfdi:ComplementoConcepto';
+}
